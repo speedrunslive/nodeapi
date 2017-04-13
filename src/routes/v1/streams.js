412fdce3422ab9f701d98274f706c829968e32d3
jiano <sunjianoman@gmail.com>
2017-04-12 22:53:12 -0400

    adding getting players by channel query param. adding delete stream - closes #14

--- src/routes/v1/streams.js
+++ src/routes/v1/streams.js
@@ -70,7 +70,69 @@ function putStreamByName(req, res, next) {
   });
 }
 
+function getPlayersByChannels(req, res, next) {
+  if (!req.params.channels || req.params.channels < 1) {
+    res.send(new errors.NotFoundError({
+      message: '`channels` is missing or blank'
+    }));
+    return;
+  } else {
+    models.Player.findAll({
+      where: {
+        '$stream.channel$': req.params.channels.split(',')
+      },
+      include: [{
+        model: models.Stream,
+        as: 'stream',
+        attributes: ['api', 'channel']
+      }],
+      attributes: ['id', 'name', 'twitter', 'youtube', 'country']
+    }).then(function(players) {
+      res.send(
+        players.map(function(player) {
+          return player.toV1JSON();
+        })
+      );
+    }).catch(function(error) {
+      res.send(error); // CLEANUP: add real error
+    });
+  }
+}
+
+function deleteStreamByName(req, res, next) {
+  if (!req.params.user || req.params.user < 1) {
+    res.send(new errors.NotFoundError({
+      message: '`user` is missing or blank'
+    }));
+    return;
+  }
+
+  findStream(req.params.user).then(function(stream) {
+    if (!stream) {
+      res.send(new errors.NotFoundError({
+        message: 'No player with name ' + req.params.user
+      }));
+    } else {
+      stream.destroy().then(function() {
+        res.send({
+          message: 'Deleted user ' + req.params.user
+        });
+      }).catch(function(error) {
+        res.send(error); // CLEANUP: add real error
+      });
+    }
+  });
+}
+
 function registerRoutes(server) {
+  server.get({
+    path: '/streams',
+    version: '1.0.0'
+  }, getPlayersByChannels);
+  server.del({
+    path: '/streams',
+    version: '1.0.0'
+  }, deleteStreamByName);
   server.get({
     path: '/streams/:name',
     version: '1.0.0'
